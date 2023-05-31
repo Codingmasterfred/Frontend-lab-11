f9f26814c913f393a2a7d3fa26c33eda2a5224c6
Codingmasterfred <fredrickg777@gmail.com>
2023-05-31 09:16:22 -0700

    frontend

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useState } from "react";
-import {Carousel} from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import axios from "axios";
-import {useEffect} from "react"
+import { useEffect } from "react"
 import { Form } from 'react-bootstrap';
 import BookForm from "./BookFormModal"
 
@@ -17,21 +17,30 @@ function BestBooks() {
   // }
   const [books, setBooks] = useState([])
   const [showForm, setShowForm] = useState(false); // State for revealing the "New Book" form
+  const [title, titlechange] = useState("")
+  const [description, descriptionchange] = useState("")
+  const [status, statuschange] = useState("")
+  const [click, clickfunction] = useState(false)
+  const [editbook,editingbook] = useState({
+    title:"",
+    description:"",
+    status:""
+  })
 
 
   useEffect(() => {
     // Define an async function
-    
-    async function componentDidMount(){
-    try{
-     let BookData = await axios.get("http://localhost:3001/books");
-     console.log("BookData",BookData.data)
-     setBooks(BookData)
-  
-    }
-    catch(error){
-      console.error(error)
-    }
+
+    async function componentDidMount() {
+      try {
+        let BookData = await axios.get("http://localhost:3001/books");
+        console.log("BookData", BookData.data)
+        setBooks(BookData)
+
+      }
+      catch (error) {
+        console.error(error)
+      }
     }
     // Call the async function
     componentDidMount();
@@ -42,56 +51,71 @@ function BestBooks() {
   };
 
 
+  console.log("Title", title)
+  console.log("description", description)
+  console.log("status", status)
+
+ 
+  
+  async function Delete(id){
+   let DeleteData = await axios.delete(`http://localhost:3001/books/${id}`)
+   setBooks(DeleteData)
+
+}
 
 
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
 
 
-  /* TODO: render all the books in a Carousel */
 
-  return (
-    <>
+/* TODO: Make a GET request to your API to fetch all the books from the database  */
+
+
+
+/* TODO: render all the books in a Carousel */
+
+return (
+  <>
+    <div>
+      <button onClick={handleAddBookClick}>Add Book</button>
+    </div>
+
+    {books.length != 0 ? (
+      <div style={{ display: "flex", alignItems: "center", height: "100vh", border: "1px solid black", height: "400px", marginBottom: "40px", justifyContent: showForm === false ? "center" : "space-between" }}>
+
+
+
+        <div style={{ width: "900px" }} id="BestBooksDiv">
+          {console.log("Books", books)}
+          <Carousel fade style={{ width: "900px", height: "300px", border: "1px solid black", background: "black", color: "white", textAlign: "center" }}>
+            {books.data.map(arr => {
+              return (
+                <Carousel.Item>
+
+                  <h2>{arr.title}</h2>
+                  <p>{arr.description}</p>
+                  <span>{arr.status}</span>
+                  <button id="delete" onClick={()=>{Delete(arr._id)}}>Delete it</button>
+                </Carousel.Item>
+              )
+            })
+            }
+          </Carousel>
+        </div>
+        <BookForm showForm={showForm} titlechange={titlechange} descriptionchange={descriptionchange} statuschange={statuschange} clickfunction={clickfunction} click={click} title={title} description={description} status={status} setBooks={setBooks}/>
+      </div>
+
+    )
+      : (
         <div>
-        <button onClick={handleAddBookClick}>Add Book</button>
-      </div>
+          <h3>No Books Found :(</h3>
+          <p>Book Carousel coming soon</p>
+        </div>
+      )}
 
-      {books.length != 0 ? (
-     <div style={{ display: "flex", alignItems: "center", height: "100vh", border: "1px solid black", height: "400px", marginBottom: "40px", justifyContent: showForm === false ? "center" : "space-between" }}>
-
-
-
-          <div style={{ width:"900px"}} id="BestBooksDiv">
-          {console.log("Books",books)}
-          <Carousel fade style={{width:"900px", height:"300px", border:"1px solid black", background:"black", color:"white",textAlign:"center"}}>
-        {books.data.map(arr =>{
-          return(
-            <Carousel.Item>
-         
-            <h2>{arr.title}</h2>
-            <p>{arr.description}</p>
-            <span>{arr.status}</span>
-            
-        </Carousel.Item>
-          )
-        })
-      }
-  </Carousel>
-      </div>
-      <BookForm showForm={showForm}/>
-      </div>
-        
-      )
-        : (
-          <div>
-            <h3>No Books Found :(</h3>
-            <p>Book Carousel coming soon</p>
-          </div>
-        )}
-        
-    </>
-  )
+  </>
+)
 }
 
 export default BestBooks;
--- src/BookFormModal.js
+++ src/BookFormModal.js
@@ -3,38 +3,102 @@ import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Form } from 'react-bootstrap';
+import axios from 'axios';
 
 
 function BookForm(props) {
 
+   
 
-    return(
+    function onchangefunctionTitle(event) {
+        props.titlechange(event.target.value)
+    };
+  
+
+    function onchangefunctionDescription(event) {
+        props.descriptionchange(event.target.value)
+    };
+  
+
+    function onchangefunctionStatus(event) {
+        props.statuschange(event.target.value)
+    };
    
     
+
+    const handleSub = (event) => {
+        event.preventDefault()
+        console.log("Submitted")
+        onSubmitFunction()
+
+        // try {
+        //     const response = await axios.post('/books', formData);
+        //     const newBook = response.data;
+        //     // Pass the new book up to the BestBooks component
+        //     props.onAddBook(newBook);
+        //     // Reset the form data
+        //     setformData({
+        //         title: '',
+        //         description: '',
+        //         status: 'Published'
+        //     });
+        // } catch (error) {
+        //     console.error('Error saving book:', error);
+        // }
+    };
+
+
+
+async function onSubmitFunction() {
+    try {
+        const response = await axios.post('http://localhost:3001/books', {
+          title: props.title,
+          description: props.description,
+          status: props.status
+        });
+        props.setBooks(response)
+  
+
+      console.log('Response:', response.data);
+      // Handle the response data as needed
+      
+      // Reset the form fields
+   
+    } catch (error) {
+        console.error('Error saving book:', error);
+        // Handle the error
+    }
+};
+
+return (
+
         props.showForm === true && (
             <div style={{ display: "flex", border: "1px solid black", justifyContent: "center", height: "400px", alignItem: "center", backgroundColor: "black", color: "white" }}>
                 {/* Render your "New Book" form component here */}
-                <Form style={{ width: "300px", borderRadius: "30%", padding: "30px", backgroundColor: "black" }}>
+                <Form style={{ width: "300px", borderRadius: "30%", padding: "30px", backgroundColor: "black" }}  onSubmit={handleSub}>
                     <Form.Group className="mb-3">
                         <Form.Label>Title</Form.Label>
-                        <Form.Control placeholder="Title" />
+                        <Form.Control placeholder="Title" id="Title" name="Title" onChange={onchangefunctionTitle} required />
+
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label>Description</Form.Label>
-                        <Form.Control placeholder="Description" />
+                        <Form.Control placeholder="Description" id="Description"  name="Description" onChange={onchangefunctionDescription} required />
                     </Form.Group>
                     <Form.Group>
-                        <Form.Label>Status</Form.Label>
-                        <Form.Select >
+                        <Form.Label id="Status"  name="Status" >Status</Form.Label>
+                        <Form.Select onChange={onchangefunctionStatus} required>
                             <option>Published</option>
                             <option>Unpublished</option>
                         </Form.Select>
+                        <button  type="submit">Submit</button>
                     </Form.Group>
                 </Form>
             </div>
-        
+
         )
-    
-)
+
+
+    )
 }
 export default BookForm
