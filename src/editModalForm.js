acec4cd31ca91eede2a04b773267325c328aa479
Codingmasterfred <fredrickg777@gmail.com>
2023-10-29 22:11:22 -0700

    eedit

--- src/editModalForm.js
+++ src/editModalForm.js
@@ -48,8 +48,8 @@ function Modalform(props) {
         props.modalshow === true && (
             <div style={{overflow:"hidden", zIndex: "4", display: "flex", border: "1px solid black", justifyContent: "center", height: "101%", alignItems: "center", width: "100%", backgroundColor: "black", color: "white", position: "absolute", top: "0" }}>
                 {/* Render your "New Book" form component here */}
-                <div style={{ height: "100%", backgroundColor: "black", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%" }}>
-                    <Form id="Form" style={{ padding: "80px", minWidth: "300px", height: "90%", width: "90%", backgroundColor: "black", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }} onSubmit={handleSub}>
+                <div style={{overflow:"hidden", height: "100%", backgroundColor: "black", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%" }}>
+                    <Form id="Form" style={{overflow:"hidden", padding: "80px", minWidth: "300px", height: "90%", width: "90%", backgroundColor: "black", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }} onSubmit={handleSub}>
                         <Form.Group style={{ width: "90%" }} className="mb-3 FormInput">
                             <Form.Label style={{ width: "100%" }}>Title</Form.Label>
                             <Form.Control style={{ width: "100%" }} placeholder="Title" id="Title" name="Title" value={props.title} onChange={onchangefunctionTitle} required />
