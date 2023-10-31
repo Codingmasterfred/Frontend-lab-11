c732cdad3ad6f0923c719f9df6a295fd681b6aa1
Codingmasterfred <fredrickg777@gmail.com>
2023-10-30 22:01:10 -0700

    nfj

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -145,7 +145,7 @@ function BestBooks(props) {
       textarea.addEventListener('blur', handleInputBlur);
     });
   } else {
-   document.addEventListener('touchmove', allowScroll, { passive: true });
+   document.addEventListener('touchmove', allowScrollScreen, { passive: true });
   }
 }, [showForm, modalshow]);
 
@@ -166,6 +166,19 @@ function BestBooks(props) {
     const isTextArea = event.target.classList.contains('form-control'); // Assuming 'form-control' is the class Bootstrap uses for textarea
 
     if (isTextArea) {
+      // Allow scrolling within the textarea
+      event.stopPropagation();
+    } else {
+      // Prevent scrolling for other elements
+      event.preventDefault();
+    }
+  }
+
+    function allowScrollScreen(event) {
+    // Determine if scrolling should be allowed within this element (Form.Control textarea)
+    const document = event.target.document // Assuming 'form-control' is the class Bootstrap uses for textarea
+
+    if (document) {
       // Allow scrolling within the textarea
       event.stopPropagation();
     } else {
