6da1da78a403fc5b0414da0927c5e8237121e154
Codingmasterfred <fredrickg777@gmail.com>
2023-10-30 21:11:11 -0700

    guvgv

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -131,7 +131,6 @@ function BestBooks(props) {
       // Add event listeners to allow scrolling within specific elements
       document.querySelectorAll('textarea').forEach((textarea) => {
         textarea.addEventListener('touchmove', allowScroll, { passive: true });
-        textarea.addEventListener('blur', handleInputBlur);
       });
     } else {
       // Remove event listeners when the form is closed
@@ -140,9 +139,10 @@ function BestBooks(props) {
         textarea.removeEventListener('touchmove', allowScroll, { passive: true });
       });
     }
-
+    
   }, [showForm, modalshow]);
-
+  
+  textarea.addEventListener('blur', handleInputBlur);
 
 
 
